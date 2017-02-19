eddce814b2b6ab7b2f6c3098f652cf6d111c54e3
joelsnaer <joelsnaer1999@hotmail.com>
2017-02-19 22:52:42 +0000

    Verkefni 3

---
+++ application/view/home/index.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <?php
+      $hallo = "<h1>Hallo heimur</h1>";
+      $loremIpsum = "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas in odio at orci pulvinar iaculis id sit amet neque. Morbi pharetra elementum finibus. Maecenas sit amet lectus at justo rhoncus commodo</p>";
+      echo $hallo;
+      echo $loremIpsum;
+    ?>
+</div>
---
+++ application/view/home/undirsida1.php
@@ -0,0 +1,8 @@
+<div class="container">
+  <?php
+    $title = "<h1>Undirsíða 1</h1>";
+    $loremIpsum = "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas in odio at orci pulvinar iaculis id sit amet neque. Morbi pharetra elementum finibus. Maecenas sit amet lectus at justo rhoncus commodo. Nunc placerat diam ut sem sagittis, at aliquam sem eleifend. Fusce aliquet mauris ut orci elementum, consectetur ullamcorper arcu pellentesque. Maecenas et mauris ipsum. Nullam imperdiet nunc ligula, sed ultrices tortor convallis eget. Vestibulum fermentum ac neque eget maximus. Praesent purus tortor, sagittis et sapien vel, finibus porttitor elit. Nulla dignissim massa nec quam vulputate, sed ultricies orci placerat. Nam sit amet feugiat enim. Ut ornare sapien a hendrerit tristique. Praesent non rhoncus sem. Fusce ut quam sit amet augue fermentum volutpat venenatis vel elit. Ut in egestas tellus, in sodales leo. Praesent condimentum metus bibendum velit sagittis malesuada.</p>";
+    echo $title;
+    echo $loremIpsum;
+  ?>
+</div>
